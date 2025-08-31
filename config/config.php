dbfd8b4724522e6ca15dec05c8901687b76e9e22
elfordanson <116512676+elfordanson@users.noreply.github.com>
2025-09-01 00:24:22 +0300

    Update config.php

--- config/config.php
+++ config/config.php
@@ -9,7 +9,7 @@
 | It can be obtained from https://telegram.dog/BotFather
 |
 */
-$config['botToken'] = "";
+$config['botToken'] = "7469560376:AAECiCuzGMmQE-sVWnSzzXxA0qFd2Z0znUg";
 
 /*
 |--------------------------------------------------------------------------
@@ -20,7 +20,7 @@ $config['botToken'] = "";
 | ID can be obtained from https://telegram.dog/username_to_id_bot
 |
 */
-$config['adminID'] = "";
+$config['adminID'] = "5595528064";
 
 /*
 |--------------------------------------------------------------------------
@@ -31,7 +31,7 @@ $config['adminID'] = "";
 | ID can be obtained from https://telegram.dog/BotFather
 |
 */
-$config['logsID'] =  "";
+$config['logsID'] =  "-1002946651819";
 
 /*
 |--------------------------------------------------------------------------
@@ -54,7 +54,7 @@ $config['timeZone'] =  "Asia/Kolkata";
 */
 $config['db']['hostname'] =  "localhost";
 $config['db']['username'] =  "root";
-$config['db']['password'] =  "";
+$config['db']['password'] =  "elfordanson";
 $config['db']['database'] =  "checkerbot";
 
 /*
@@ -78,5 +78,5 @@ $config['anti_spam_timer'] =  "20";
 | array('sk1','sk2','sk3')
 |
 */
-$config['sk_keys'] =  array('sk_live_69GKI0saLB8uIEnxzv8VTvRX');
+$config['sk_keys'] =  array('sk_test_51S0Ia1LDiu5ojaiMoOzxjwm8ISwkb04zWuOieA3M7PbpZwX64elrBHdQTdBrFwMptCK25Kb7M8kQvDnWZn7PgnAS00nYSLLxKr');
 ?>
